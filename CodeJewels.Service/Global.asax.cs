193328aa037d6c1f16b78efa271e78d852414900
smo82 <smo@gbg.bg>
2013-08-19 00:38:07 +0300

    Empty commit

--- CodeJewels.Service/Global.asax.cs
+++ CodeJewels.Service/Global.asax.cs
@@ -11,7 +11,7 @@ using System.Web.Routing;
 namespace CodeJewels.Service
 {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
+    // visit http://go.microsoft.com/?LinkId=9394801 
 
     public class WebApiApplication : System.Web.HttpApplication
     {
